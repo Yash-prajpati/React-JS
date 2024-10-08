de4ad97213d41ab6180308f350d74686c39149c6
Yash-prajapati <yashprajapati290208@gmail.com>
2024-10-08 10:31:04 +0530

    19

---
+++ Website/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./parts/Header"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+
+function App() {
+    return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
