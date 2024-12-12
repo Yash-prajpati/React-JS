5dd2f20c4d2df5c43fcf484985b20f41a2981869
Yash-prajapati <yashprajapati290208@gmail.com>
2024-12-12 16:08:52 +0530

    12/12/2024

--- Exam-pr.2/src/App.jsx
+++ Exam-pr.2/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
- 
-  return (
-  <>
-  </>
-  )
-}
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import SignUp from "./SignUp";
+import TodoApp from "./TodoItem";
 
-export default App
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/todos" element={<TodoApp />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
